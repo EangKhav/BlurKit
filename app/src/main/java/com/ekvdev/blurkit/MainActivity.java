a96f054f1905db3e49dec3e0adce2ef756bd3b8e
Eang Khav <EKV@EKV-MacBookPro.local>
2020-10-22 13:05:18 +0700

    init

---
+++ app/src/main/java/com/ekvdev/blurkit/MainActivity.java
@@ -0,0 +1,14 @@
+package com.ekvdev.blurkit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
